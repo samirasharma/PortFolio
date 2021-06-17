2dbd9446ec3808cf5c5ef9222f65c0377275601b
samirasharma <samira.sharma@outlook.com>
2021-06-17 13:02:53 -0500

    addedinformation

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,19 +6,20 @@ import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
-  return (
-    <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Saad Pasta")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
-          </a>
-        </p>
-      </div>
-    </Fade>
-  );
+  return null;
+//  return (
+//   <Fade bottom duration={1000} distance="0px">
+//      <div className="footer-div">
+//        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+//          {emoji("Made with ❤️ by Saad Pasta")}
+//        </p>
+//        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+//          Theme by{" "}
+//          <a href="https://github.com/saadpasta/developerFolio">
+//            developerFolio
+//          </a>
+//        </p>
+//      </div>
+//    </Fade>
+//);
 }
--- src/portfolio.js
+++ src/portfolio.js
@@ -11,27 +11,23 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Samira Sharma",
+  title: "Hi, I'm Samira",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Sr. Technology Professional with a hands-on experience in python, bash scripting, developing desktop and mobile applications. Skilled in configuring and maintaining CI/CD pipeline using Jenkins, Ansible, Docker, Kubernetes. Also, capable of setting up infrastructure in AWS, such as:  EC2, S3, Route53, RDS, etc. as well as configuring hadoop cluster and developing spark(python) application in distributed environment."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+    "https://drive.google.com/file/d/1eT3ISE1iolKQWX9ZbI88D4J9gDQ_Zcka/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram and Twitter are also supported in the links!
+  github: "https://github.com/samirasharma",
+  linkedin: "https://www.linkedin.com/in/samira-sharma-1134b2a3/",
+  gmail: "samira.sharmaaa@gmail.com",
+  // Instagram and Twitter are also supported in the links !
   display: true // Set true to display this section, defaults to false
 };
 
@@ -39,72 +35,75 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "TECHNOLOGY ENTHUSIAST WITH GREAT INTEREST IN CLOUD/DEVOPS/DATA SCIENCE FIELD",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Execute projects and/or support tasks while maintaining a high level of direct client interaction"
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Manage production system in a distributed and highly available critical infrastructure"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Install, Configure and maintain CI/CD using Jenkins"),
+    emoji(
+      "⚡ Develop spark application in python in standalone as well as distributed environment"
     )
   ],
 
-  /* Make Sure to include correct Font Awesome Classname to view your icon
+  /* Make sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
+      skillName: "Python",
       fontAwesomeClassname: "fab fa-python"
     },
     {
-      skillName: "docker",
+      skillName: "Linux",
+      fontAwesomeClassname: "fab fa-linux"
+    },
+    {
+      skillName: "Github",
+      fontAwesomeClassname: "fab fa-github"
+    },
+    {
+      skillName: "Jenkins",
+      fontAwesomeClassname: "fab fa-jenkins"
+    },
+    
+    {
+      skillName: "Hadoop",
+      fontAwesomeClassname: "fas fa-server"
+    },
+    {
+      skillName: "AWS",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "Docker",
       fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "Oracle",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "MongoDB",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "Android",
+      fontAwesomeClassname: "fab fa-android"
+    },
+    {
+      skillName: "IONIC",
+      fontAwesomeClassname: "fab fa-angular"
+    },
+    {
+      skillName: "HTML",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "R",
+      fontAwesomeClassname: "fab fa-r-project"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -116,23 +115,27 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
+      schoolName: "New Jersey Institute of Technology",
+      logo: require("./assets/images/NJIT-logo.png"),
       subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      duration: "Jan 2020 - Aug 2021",
+      desc: "Focus on Data & Cloud courses and projects- Cloud Computing, Data Analytics with R, Advanced Database System Design, Data Mining, Introduction to Big Data, Data Structure and Algorithm",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Developed a project to analyse stream of tweets on twitter and display their sentiments using Tweepy, Pyspark, Spark Streaming, SQL",
+        "Built a parallel processing project where one instance reads images, detects label and stores information in sqs queue. Next instance picks up index of image and detects text.Used Java, Amazon Rekognition, SQS, S3, EC2",
+	"Developed wine quality predictor application that run in AWS distributed system using Python, Hadoop-Spark, Python"
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "National College Of Engineering, Tribhuvan University,Nepal",
+      logo: require("./assets/images/NCE-logo.png"),
+      subHeader: "Bachelor of Electronics and Computer Engineering",
+      duration: "Nov 2011 - Dec 2015",
+      desc: "Fellowship and Regular scholarship during the 4-year study along with College Outstanding Award and top rank. Took courses related to DBMS, Artificial Intelligence, Data Mining, C, C++, Networking",
+      descBullets: [
+	"Project developed to monitor and control the switching of household devices with Android as well as Web application globally using Internet. Android application and Web applications were synchronized and Arduino microcontroller was used to physically turn the device on or off. IDE such as: Android Studio, Arduino, Netbeans were used to develop applications",
+	"Second Prize winner in the ChildApp Appathon organized by UNICEF, Microsoft Innovation Center and ChildReach,Nepal.Main Objective of this project was to provide interactive learning environment to connect students with their course through game based learning. Motion sensor of Microsoft Kinect Technology was programmed to detect the gestures of user and C# programming was used to develop an application"
+      ]
     }
   ]
 };
@@ -143,15 +146,15 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      Stack: "CI/CD Pipelines", //Insert stack or technology you have experience in
+      progressPercentage: "70%" //Insert relative proficiency in percentage
     },
     {
-      Stack: "Backend",
-      progressPercentage: "70%"
+      Stack: "Cloud Design and Architecture",
+      progressPercentage: "75%"
     },
     {
-      Stack: "Programming",
+      Stack: "Scripting and Programming",
       progressPercentage: "60%"
     }
   ],
@@ -164,29 +167,44 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Sr. Technology Professional",
+      company: "Public Service Enterprise Group (PSEG)",
+      companylogo: require("./assets/images/pseg-logo-png-transparent.png"),
+      date: "September 2019 – Present",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Responsible for executing projects and/or support tasks and maintain a high level of direct client interaction",
+	" Responsible for operating and maintaining production systems",
+        " Automate daily jobs using python and PowerShell scripts",
+	" Hands-on experience with IBM WebSphere Application Server for both standalone server as well as clusters",
+	" Responsible for coordinating the implementation of multiple SAAS solutions for existing applications",
+	" Analyze inquiries, determine appropriate technical area or vendor to resolve problems, and make suggestions for improvements"
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "AWS DevOps Engineer",
+      company: "CloudChomp Inc.",
+      companylogo: require("./assets/images/cloudchomp-logo.png"),
+      date: "August 2018 – May 2019",
+      descBullets: [
+        " Developed and maintained automated CI/CD pipelines for code deployment using Jenkins",
+	" Actively managed, improved and monitored cloud infrastructure on AWS",
+	" Performed code merges and deployments with DEV and QA teams",
+	" Wrote Ansible playbooks to manage Web applications while deploying the necessary changes and monitored the process",
+	" Wrote scripts in Python and Bash to automate various processes, such as: rotating AWS access key, take snapshots of AWS EC2 servers, provide DB read/write/all permissions"
+      ]
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "DevOps/Computer Engineer",
+      company: "Bajra Technologies Pvt. Ltd.",
+      companylogo: require("./assets/images/bajratechnologies-logo.png"),
+      date: "April 2016 – April 2018",
+      descBullets: [
+	 " Built & configured Linux systems over the network, implemented automated tasks through crontab",
+	 " Migrated VMWARE VMs to AWS and Managed Services like EC2, S3 Bucket, Route53, ELB, EBS",
+	 " Worked on Angular framework and had experience working with Ng-Modules, components, Observables, Ng-router, AUTH-guards", 
+	 " Developed a project using IONIC framework. Created GUI based on layout, maintained database (SQLITE) to store information and sync data to server",
+	 " Installed multiple instances of Apache Tomcat and helped with the deployment of JARs and WARs on development environments"
+      ]
     }
   ]
 };
@@ -196,7 +214,7 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
@@ -229,7 +247,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
@@ -238,51 +256,30 @@ const bigProjects = {
 const achievementSection = {
   title: emoji("Achievements And Certifications 🏆 "),
   subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+    "Achievements and Certifications that I received !",
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Microsoft Certified Professional",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
+        "Server Virtualization With Windows Server Hyper-V and System Center",
+      image: require("./assets/images/microsoft-certified-professional-logo.png"),
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          url: "https://drive.google.com/file/d/1gArp4yzAiEOiFnoAH59PeRzFESvSoylA/view?usp=sharing"
         }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Microsoft Student Partner",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
+        "Microsoft Innovation Center",
+      image: require("./assets/images/msp-logo.png"),
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
-        }
-      ]
-    },
-
-    {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      footerLink: [
-        {name: "Certification", url: ""},
-        {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Achievement",
+          url: "https://drive.google.com/file/d/0Bwg4gVyssGr9RE4wOVF3UzNCVWM/view?usp=sharing&resourcekey=0-QNhHM2_WeFl6jpCzgf_BBw"
         }
       ]
     }
@@ -311,7 +308,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -330,7 +327,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -343,22 +340,22 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3243454077",
-  email_address: "saadpasta70@gmail.com"
+    "Want to say hi or have a discussion ? Please reach out to me.",
+  number: "+1609-972-2580",
+  email_address: "samira.sharmaaa@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 export {
